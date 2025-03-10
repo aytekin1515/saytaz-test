ab2f6cbdeb0af88a4765820a4187d0374df9e5d4
Aytekin <aytkinvliyeva32@gmail.com>
2025-03-10 19:34:37 +0400

    birinci kommit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{htm,js}"],
+  theme: {
+    container: {
+      'sm': '450px',
+     
+      'md': '768px',
+      
+      'lg': '1024px',
+     
+      'xl': '1280px',
+
+      '2xl': '1536px',
+    }, 
+    extend: {},
+  },
+  plugins: [],
+}
+
